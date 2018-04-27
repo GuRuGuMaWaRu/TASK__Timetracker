0c233f1990f0754e34dd20c33bb04cb33e974af2
Peter Krevenets <peter.k.nets@gmail.com>
2018-04-27 10:48:27 +0300

    Add date data in a separate data file

---
+++ client/src/utils/dateData.js
@@ -0,0 +1,29 @@
+export const daysOfWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+export const daysInMonths = {
+  January: 31,
+  February: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+};
