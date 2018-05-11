27622f187ce4cefb56916b125da5d6dc2320bea8
Peter Krevenets <peter.k.nets@gmail.com>
2018-05-11 13:06:49 +0300

    Plug calendarReducer into root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import calendarReducer from "./calendarReducer";
 import monthTasksReducer from "./monthTasksReducer";
 import timeReducer from "./timeReducer";
 import idReducer from "./idReducer";
 import tasksReducer from "./tasksReducer";
 
 export default combineReducers({
+  calendar: calendarReducer,
   monthTasks: monthTasksReducer,
   time: timeReducer,
   timerID: idReducer,
