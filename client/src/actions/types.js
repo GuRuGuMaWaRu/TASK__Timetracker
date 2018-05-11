38916c3459fcc4cb4666c289690f1a6b6fe80b3f
Peter Krevenets <peter.k.nets@gmail.com>
2018-05-11 14:02:13 +0300

    Create DateSelector separate component

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,3 +4,4 @@ export const GET_MONTH = "get_month";
 export const UPDATE_TIMER = "update_timer";
 export const CLEAR_TIMER = "clear_timer";
 export const SET_TIMER_ID = "set_timer_id";
+export const CHANGE_DATE = "change_date";
