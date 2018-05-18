4607a4831981902e6c16cb5685813c87da3ee05c
Peter Krevenets <peter.k.nets@gmail.com>
2018-05-18 18:05:40 +0300

    Add network error handlers --- no more strange network errors!

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,5 +1,4 @@
 // Asyn/Await Error Handler
-
 exports.catchErrors = fn => {
   return function(req, res, next) {
     return fn(req, res, next).catch(next);
@@ -7,9 +6,36 @@ exports.catchErrors = fn => {
 };
 
 // Not Found Error Handler
-
 exports.notFound = (req, res, next) => {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
+};
+
+// Other error handlers
+exports.developmentErrors = (err, req, res, next) => {
+  err.stack = err.stack || "";
+
+  const errorDetails = {
+    status: err.status,
+    message: err.message,
+    stackHighlighted: err.stack.replace(
+      /[a-z_-\d]+.js:\d+:\d+/gi,
+      "<mark>$&</mark>"
+    )
+  };
+
+  res.status(err.status || 500);
+  res.json(errorDetails);
+};
+
+exports.productionErrors = (err, req, res, next) => {
+  res.status(err.status || 500);
+
+  const errorDetails = {
+    status: err.status,
+    message: err.message
+  };
+
+  res.json(errorDetails);
 };
