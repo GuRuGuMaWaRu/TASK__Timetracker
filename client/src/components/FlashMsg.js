7df8166db2a4af2e0789bfa7bf48d34012e8ced6
Peter Krevenets <peter.k.nets@gmail.com>
2018-05-21 14:26:33 +0300

    Use react-transition to animate message fade-in-out

--- client/src/components/FlashMsg.js
+++ client/src/components/FlashMsg.js
@@ -1,34 +1,62 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
 
 import * as actions from "../actions";
-import "./FlashMsg.css";
+// import "./FlashMsg.css";
 
+const styles = theme => ({
+  flashMessage: {
+    position: "absolute",
+    bottom: "20px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    padding: "8px 20px",
+    borderRadius: "13px",
+    color: "#fff",
+    backgroundColor: "slategrey",
+    transition: "opacity 500ms ease-in-out",
+    opacity: 0
+  },
+  entering: {
+    opacity: 0
+  },
+  entered: {
+    opacity: 1
+  },
+  exiting: {
+    opacity: 1
+  },
+  exited: {
+    opacity: 0
+  }
+});
 class flashMsg extends Component {
-  state = {
-    timer: 0
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    status: PropTypes.string.isRequired,
+    hideAddMsg: PropTypes.func.isRequired
   };
 
-  componentDidMount() {
-    const timer = setTimeout(this.props.hideAddMsg, 2000);
-
-    this.setState({
-      timer
-    });
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.state.timer);
-  }
-
   render() {
+    const { classes, status } = this.props;
+
+    // hide message in 2 seconds
+    if (status === "entered") {
+      setTimeout(this.props.hideAddMsg, 2000);
+    }
+
     return (
-      <Typography type="div" className="flash-message">
+      <Typography
+        type="div"
+        className={`${classes.flashMessage} ${classes[status]}`}
+      >
         Congratulations! You've added a new task
       </Typography>
     );
   }
 }
 
-export default connect(null, actions)(flashMsg);
+export default connect(null, actions)(withStyles(styles)(flashMsg));
