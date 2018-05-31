57d69b3cae6dee338a182fbb79cb79e70d9c3125
Peter Krevenets <peter.k.nets@gmail.com>
2018-05-31 20:42:46 +0300

    Try to create Modal

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { withStyles } from "material-ui/styles";
 
 import Header from "./Header";
--- client/src/components/Temp/TimerBook.js
+++ client/src/components/Temp/TimerBook.js
@@ -3,8 +3,17 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+/////////////////////////
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+/////////////////////////
 
 import * as actions from "../../actions";
+import Modal from "./Modal";
 
 const styles = theme => ({
   button: {
@@ -16,13 +25,61 @@ const styles = theme => ({
 });
 
 class TimerBook extends Component {
+  state = {
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, toggleModal, modalOpen } = this.props;
 
     return (
-      <Button variant="outlined" color="secondary" className={classes.button}>
-        book
-      </Button>
+      <div>
+        <Button
+          variant="outlined"
+          color="secondary"
+          className={classes.button}
+          onClick={this.handleClickOpen}
+        >
+          book
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
   }
 }
@@ -31,4 +88,4 @@ TimerBook.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default connect(null, actions)(withStyles(styles)(TimerBook));
+export default withStyles(styles)(TimerBook);
