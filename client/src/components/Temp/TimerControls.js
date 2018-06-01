73aba96c8d85c08c6ca8ca6486bb8bd3a7e1fbfd
Peter Krevenets <peter.k.nets@gmail.com>
2018-06-01 11:26:55 +0300

    Refactor TimerControls into functional component

--- client/src/components/Temp/TimerControls.js
+++ client/src/components/Temp/TimerControls.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
@@ -21,68 +21,40 @@ const styles = theme => ({
   }
 });
 
-class TimerControls extends Component {
-  // toggleTimer = () => {
-  //   if (!this.props.timerIsRunning) {
-  //     const startTime = Date.now() - this.props.time * 1000;
-
-  //     const timerID = setInterval(() => {
-  //       const currentTime = Math.round((Date.now() - startTime) / 1000);
-  //       this.props.updateTimer(currentTime);
-  //     }, 1000);
-
-  //     this.props.setTimerID(timerID);
-  //   } else {
-  //     clearInterval(this.props.timerID);
-  //     this.props.stopTimer();
-  //   }
-  // };
-
-  // handleClear = () => {
-  //   clearInterval(this.props.timerID);
-  //   this.props.clearTimer();
-  // };
-
-  render() {
-    const {
-      classes,
-      timerIsRunning,
-      startTimer,
-      clearTimer,
-      stopTimer
-    } = this.props;
-
-    return (
-      <div className={classes.timerControls}>
-        <IconButton
-          className={classes.button}
-          color="primary"
-          aria-label={timerIsRunning ? "Run" : "Pause"}
-          onClick={() => {
-            timerIsRunning ? stopTimer() : startTimer();
-          }}
-        >
-          {timerIsRunning ? <Pause /> : <PlayArrow />}
-        </IconButton>
-        <IconButton
-          className={classes.button}
-          color="secondary"
-          aria-label="Clear"
-          onClick={clearTimer}
-        >
-          <Clear />
-        </IconButton>
-      </div>
-    );
-  }
-}
+const TimerControls = ({
+  classes,
+  timerIsRunning,
+  startTimer,
+  clearTimer,
+  stopTimer
+}) => {
+  return (
+    <div className={classes.timerControls}>
+      <IconButton
+        className={classes.button}
+        color="primary"
+        aria-label={timerIsRunning ? "Run" : "Pause"}
+        onClick={() => {
+          timerIsRunning ? stopTimer() : startTimer();
+        }}
+      >
+        {timerIsRunning ? <Pause /> : <PlayArrow />}
+      </IconButton>
+      <IconButton
+        className={classes.button}
+        color="secondary"
+        aria-label="Clear"
+        onClick={clearTimer}
+      >
+        <Clear />
+      </IconButton>
+    </div>
+  );
+};
 
 TimerControls.propTypes = {
   classes: PropTypes.object.isRequired,
   timerIsRunning: PropTypes.bool.isRequired,
-  // timerID: PropTypes.number.isRequired,
-  // updateTimer: PropTypes.func.isRequired,
-  // setTimerID: PropTypes.func.isRequired,
   startTimer: PropTypes.func.isRequired,
   clearTimer: PropTypes.func.isRequired,
   stopTimer: PropTypes.func.isRequired
