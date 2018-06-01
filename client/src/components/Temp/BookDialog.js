7b58deedfeed4606aceba4b9115d46ed750bafe6
Peter Krevenets <peter.k.nets@gmail.com>
2018-06-01 12:27:41 +0300

    Start changing dummy BookDialog into a real one

--- client/src/components/Temp/BookDialog.js
+++ client/src/components/Temp/BookDialog.js
@@ -1,42 +1,57 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog, {
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle
 } from "material-ui/Dialog";
+
+import * as actions from "../../actions";
+import { showTime } from "../../utils/timer";
 class BookDialog extends Component {
+  state = {
+    time: showTime(this.props.time)
+  };
+
   render() {
     const { isOpen, handleClose } = this.props;
     return (
-      <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+      <Dialog open={isOpen} onClose={handleClose} aria-labelledby="book-time">
+        <DialogTitle id="book-time">Book Time</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
+            id="time"
+            label="Time"
+            type="time"
+            InputLabelProps={{
+              shrink: true
+            }}
+            fullWidth
+            value={this.state.time}
+          />
+          <TextField
+            margin="dense"
+            id="description"
+            label="Description"
+            type="string"
+            InputLabelProps={{
+              shrink: true
+            }}
+            multiline={true}
             fullWidth
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            Cancel
+            Book
           </Button>
           <Button onClick={handleClose} color="primary">
-            Subscribe
+            Cancel
           </Button>
         </DialogActions>
       </Dialog>
@@ -44,4 +59,12 @@ class BookDialog extends Component {
   }
 }
 
-export default BookDialog;
+BookDialog.propTypes = {
+  time: PropTypes.number.isRequired
+};
+
+const mapStateToProps = ({ time }) => ({
+  time
+});
+
+export default connect(mapStateToProps, actions)(BookDialog);
