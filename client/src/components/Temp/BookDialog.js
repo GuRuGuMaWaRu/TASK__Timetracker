8fcdc92a118c6676de85a9d764f12714917203de
Peter Krevenets <peter.k.nets@gmail.com>
2018-06-02 10:49:53 +0300

    Update description in BookDialog using internal state

--- client/src/components/Temp/BookDialog.js
+++ client/src/components/Temp/BookDialog.js
@@ -12,12 +12,19 @@ import Dialog, {
 import * as actions from "../../actions";
 class BookDialog extends Component {
   state = {
-    errorTime: 0,
-    errorDescription: 0
+    errorTime: false,
+    errorDescription: false,
+    description: ""
   };
 
   handleChangeTime = e => {
     this.props.updateEdit(e.target.value);
+  };
+
+  handleChangeDescription = e => {
+    this.setState({
+      description: e.target.value
+    });
   };
 
   render() {
@@ -38,8 +45,8 @@ class BookDialog extends Component {
             fullWidth
             value={editTime}
             onChange={this.handleChangeTime}
-            error={this.state.errorTime === 1}
-            helperText={this.state.errorTime === 1 ? "Please enter time" : ""}
+            error={this.state.errorTime}
+            helperText={this.state.errorTime ? "Please enter time" : ""}
           />
           <TextField
             margin="dense"
@@ -51,6 +58,12 @@ class BookDialog extends Component {
             }}
             multiline={true}
             fullWidth
+            value={this.state.description}
+            onChange={this.handleChangeDescription}
+            error={this.state.errorDescription}
+            helperText={
+              this.state.errorDescription ? "Please enter description" : ""
+            }
           />
         </DialogContent>
         <DialogActions>
