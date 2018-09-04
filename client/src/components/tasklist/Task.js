1982a529c6c5e2b11a316901e57e2416a54d29e6
Peter Krevenets <peter.k.nets@gmail.com>
2018-09-04 15:14:05 +0300

    Connect styles for individual tasks

--- client/src/components/tasklist/Task.js
+++ client/src/components/tasklist/Task.js
@@ -1,9 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
-const Task = ({ task }) => {
+const Task = ({ classes, task }) => {
   const { description } = task;
 
-  return <div>{description}</div>;
+  return <div className={classes.task}>{description}</div>;
 };
 
-export default Task;
+const styles = theme => ({
+  task: {
+    color: "red"
+  }
+});
+
+Task.propTypes = {
+  classes: PropTypes.object.isRequired,
+  task: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    month: PropTypes.number.isRequired,
+    day: PropTypes.number.isRequired,
+    time: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired
+  })
+};
+
+export default withStyles(styles)(Task);
