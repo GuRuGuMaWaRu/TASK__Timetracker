0eb47eec118051feb14a641cc7d1be0b468d6808
Peter Krevenets <peter.k.nets@gmail.com>
2018-10-22 17:26:51 +0300

    Show a template tooltip & remove hover animation

--- client/src/components/Calendar/CalendarCard.js
+++ client/src/components/Calendar/CalendarCard.js
@@ -6,6 +6,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
 
 import "../calendar/Calendar.css";
 import * as actions from "../../actions";
@@ -30,11 +31,20 @@ class CalendarCard extends React.Component {
 
   constructMonth = () => {
     return this.props.displayedMonth.map(cell => {
+      if (cell.withTasks) {
+        return (
+          <Tooltip
+            key={`${cell.className}-${cell.value}`}
+            placement="top"
+            title="wow"
+          >
+            <div className={`${cell.className} with-tasks`}>{cell.value}</div>
+          </Tooltip>
+        );
+      }
+
       return (
-        <div
-          key={`${cell.className}-${cell.value}`}
-          className={`${cell.className} ${cell.withTasks ? "with-tasks" : ""}`}
-        >
+        <div key={`${cell.className}-${cell.value}`} className={cell.className}>
           {cell.value}
         </div>
       );
