5313df1245f8341e13f1d7f6171e59e8a10511de
Peter Krevenets <peter.k.nets@gmail.com>
2018-11-07 12:07:54 +0200

    Place Calendar button together with Book button

--- client/src/components/timer/Book.js
+++ client/src/components/timer/Book.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import compose from "recompose/compose";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import DateRangeIcon from "@material-ui/icons/DateRange";
 
 import BookDialog from "./BookDialog";
 import { stopTimer, startEdit } from "../../actions";
@@ -29,7 +31,11 @@ class Book extends Component {
     const { classes } = this.props;
 
     return (
-      <div>
+      <div className={classes.view}>
+        <IconButton className={classes.calendarButton} aria-label="Calendar">
+          <DateRangeIcon />
+        </IconButton>
+
         <Button
           variant="outlined"
           color="secondary"
@@ -45,12 +51,21 @@ class Book extends Component {
 }
 
 const styles = theme => ({
+  view: {
+    display: "flex"
+  },
   button: {
     margin: theme.spacing.unit,
     [theme.breakpoints.down("xs")]: {
       margin: 0,
       padding: 0,
       minWidth: "60px"
+    }
+  },
+  calendarButton: {
+    display: "none",
+    [theme.breakpoints.down("sm")]: {
+      display: "block"
     }
   }
 });
