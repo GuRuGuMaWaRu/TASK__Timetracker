1593d88099eee151fa1c4c2141fbf6b047dff168
GuRuGu <peter.k.nets@gmail.com>
2019-07-11 17:26:44 +0300

    Test --- remove everything from client and load only a simple div

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,36 +11,38 @@ import "./App.css";
 import { getTasksPage, setPageLimit } from "../actions";
 import { maxTasksPerPage } from "../utils/tasks";
 
-class App extends Component {
-  componentDidMount() {
-    // 1. Find max number of tasks per page
-    const limit = maxTasksPerPage();
-    // 2. Save max number of tasks in state store
-    this.props.setPageLimit(limit);
-    // 3. Get the 1st page with tasks per page depending on screen size
-    this.props.getTasksPage(1);
+// class App extends Component {
+//   componentDidMount() {
+//     // 1. Find max number of tasks per page
+//     const limit = maxTasksPerPage();
+//     // 2. Save max number of tasks in state store
+//     this.props.setPageLimit(limit);
+//     // 3. Get the 1st page with tasks per page depending on screen size
+//     this.props.getTasksPage(1);
 
-    // window.onresize = event => {
-    //   if (window.innerHeight > window.innerWidth) {
-    //     alert("You are now in portrait");
-    //   } else {
-    //     alert("You are now in landscape");
-    //   }
-    // };
-  }
+//     // window.onresize = event => {
+//     //   if (window.innerHeight > window.innerWidth) {
+//     //     alert("You are now in portrait");
+//     //   } else {
+//     //     alert("You are now in landscape");
+//     //   }
+//     // };
+//   }
 
-  render() {
-    const { classes } = this.props;
+//   render() {
+//     const { classes } = this.props;
 
-    return (
-      <div className={classes.app}>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className={classes.app}>
+//         <Header />
+//         <Main />
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => <div>App</div>;
 
 const styles = theme => ({
   app: {
